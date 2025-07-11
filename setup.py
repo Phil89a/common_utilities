e024967306160e6c1123b9f18ad433338d4cc645
Phil Corley <mail@philcorley.com>
2025-07-11 20:56:37 +0100

    Version 0.2 - Added mqtt_sensor_payload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="common_utilities",
-    version="0.1.0",
+    version="0.2.0",
     description="Shared logging and utilities for my energy projects",
     author="Phil",
     packages=find_packages(),
